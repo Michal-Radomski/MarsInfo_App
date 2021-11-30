cfd846faa792bdd54bfc3acc5e3ab783844eef25
Michal Radomski <michalra@op.pl>
2021-11-30 23:24:21 +0100

    Navbar - initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import {Link} from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/money">Money</Link>
+      </li>
+      <li>
+        <Link to="/covid">Covid</Link>
+      </li>
+      <li>
+        <Link to="/Weather">Weather</Link>
+      </li>
+    </div>
+  );
+};
+export default Navbar;

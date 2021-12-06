2234f1f1edc37e85682609a54c538e6fa13d974a
Michal Radomski <michalra@op.pl>
2021-12-06 22:47:17 +0100

    Navbar - commit2

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 import "./App.scss";
-import Navbar from "./components/Navbar";
-import Home from "./components/Home";
-import Money from "./components/Money";
-import Covid from "./components/Covid";
-import Weather from "./components/Weather";
+import Navbar from "./components/navbar/Navbar";
+import Home from "./components/pages/Home";
+import Money from "./components/pages/Money";
+import Covid from "./components/pages/Covid";
+import Weather from "./components/pages/Weather";
 
 function App() {
   return (

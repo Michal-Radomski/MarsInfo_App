7b4cf8e3446979609e1d438400a5510ba5c26129
Michal Radomski <michalra@op.pl>
2021-12-16 00:14:36 +0100

    Leaflet map - commit2

--- src/components/pages/MarsWeather/MarsMap.tsx
+++ src/components/pages/MarsWeather/MarsMap.tsx
@@ -1,17 +1,20 @@
-import React from "react";
 import {MapContainer, TileLayer, Marker, Popup, Tooltip} from "react-leaflet";
 import styled from "styled-components";
 
 const DivMap = styled.div`
-  width: 500px !important;
-  height: 280px !important;
-  margin: 8px 16px !important;
+  position: absolute;
+  top: 200px;
+  left: 50px;
+  right: 0;
+  bottom: 0;
+  margin: 30px;
 `;
 
-const defaultPosition = [51.505, -0.09];
+type Position = [number, number];
+const defaultPosition: Position = [51.505, -0.09];
 console.log("defaultPosition:", defaultPosition);
 
-const MarsMap = () => {
+const MarsMap = (): JSX.Element => {
   return (
     <DivMap>
       <MapContainer center={defaultPosition} zoom={13} scrollWheelZoom={false} style={{width: "100%", height: "100%"}}>

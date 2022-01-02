c33ef300ca8e738328b4b662844331f3c10b8126
Michal Radomski <michalra@op.pl>
2022-01-02 03:59:37 +0100

    MarsMap legend - commit4

--- src/components/pages/MarsWeather/MarsLegend.jsx
+++ src/components/pages/MarsWeather/MarsLegend.jsx
@@ -3,7 +3,7 @@ import * as L from "leaflet";
 import "./Legend.css";
 
 function MarsLegend({map}) {
-  console.log("map:", map);
+  // console.log("map:", map);
   React.useEffect(() => {
     if (map) {
       const legend = L.control({position: "topright"});
@@ -11,6 +11,12 @@ function MarsLegend({map}) {
       legend.onAdd = () => {
         const div = L.DomUtil.create("div", "info legend");
         div.innerHTML = "<h4>This is the legend</h4> <br/><b>0=610 Pa</b>";
+        const img = L.DomUtil.create("img", "image");
+        img.src = "./Images/MOLA_elevation_key.png";
+        img.style.width = "512px";
+        img.style.height = "84px";
+        div.appendChild(img);
+
         return div;
       };
 

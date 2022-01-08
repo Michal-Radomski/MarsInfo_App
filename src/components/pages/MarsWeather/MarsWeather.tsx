806129641a6a09232ee190eebe1fd174833cd495
Michal Radomski <michalra@op.pl>
2022-01-08 01:20:09 +0100

    Paranell fetchting done

--- src/components/pages/MarsWeather/MarsWeather.tsx
+++ src/components/pages/MarsWeather/MarsWeather.tsx
@@ -27,10 +27,19 @@ class MarsWeather extends React.Component<Props, State> {
     };
   }
 
+  InSight_fetching = (fetchObj: any) => {
+    const solKeys = fetchObj?.sol_keys ?? "No Data";
+    // console.log("solKeys:", solKeys);
+    const solKeysMax = Math.max.apply(null, solKeys).toString();
+    // console.log("solKeysMax:", solKeysMax);
+    const result = fetchObj[solKeysMax];
+    // console.log("result:", result);
+    return result;
+  };
+
   componentDidMount() {
     const fetchMarsWeather = async () => {
       try {
-        // const response = await axios.all(URLs.map((url) => axios.get(url))).then((data) => {
         const response = await axios.all(URLs.map((url) => axios.get(url))).then((data) => {
           // console.log("data:", data);
           return data;
@@ -41,7 +50,7 @@ class MarsWeather extends React.Component<Props, State> {
         const marsWeatherModified = {
           PerseveranceWeather: marsWeathers[0].sols,
           CuriosityWeather: marsWeathers[1].soles.slice(0, 7).reverse(),
-          InSightWeather: marsWeathers[2],
+          InSightWeather: this.InSight_fetching(marsWeathers[2]),
         };
         // await console.log("marsWeatherModified:", marsWeatherModified);
         await this.setState({
@@ -50,8 +59,7 @@ class MarsWeather extends React.Component<Props, State> {
           InSightWeather: marsWeatherModified.InSightWeather,
           loaded: true,
         });
-
-        // console.log("this.state:", this.state);
+        await console.log("this.state:", this.state);
       } catch (error) {
         console.error(error);
       }
@@ -80,65 +88,3 @@ class MarsWeather extends React.Component<Props, State> {
 }
 
 export default MarsWeather;
-
-//+ Old version
-// import React from "react";
-// import axios from "axios";
-
-// import MarsMap from "./MarsMap";
-// import EarthMap from "./EarthMap";
-// import Mars from "./CurrentWeather/Mars";
-
-// const Mars2020_URL = process.env.REACT_APP_MARS2020_API as string;
-// // console.log("Mars2020_URL:", Mars2020_URL);
-// const MSL_URL = process.env.REACT_APP_MSL_API as string;
-// // console.log("MSL_URL:", MSL_URL);
-// const InSight = process.env.REACT_APP_InSight_API as string;
-// // console.log("InSight:", InSight);
-
-// declare interface Props {}
-
-// class MarsWeather extends React.Component<Props, State> {
-//   constructor(props: Props) {
-//     super(props);
-//     this.state = {
-//       marsWeather: null,
-//       marsWeatherLast: null,
-//       loaded: false,
-//     };
-//   }
-
-//   componentDidMount() {
-//     const fetchMarsWeather = async () => {
-//       try {
-//         const response = await axios.get(Mars2020_URL);
-//         const marsWeather = response.data;
-//         const marsWeatherLast = response.data.sols[6];
-//         // console.log("marsWeather:", marsWeather);
-//         // console.log("marsWeatherLast:", marsWeatherLast);
-//         await this.setState({marsWeather: marsWeather, marsWeatherLast: marsWeatherLast, loaded: true});
-//         // console.log(this.state);
-//       } catch (error) {
-//         console.error(error);
-//       }
-//     };
-//     fetchMarsWeather();
-//   }
-
-//   render() {
-//     if (!this.state.loaded) {
-//       return <div>Loading...</div>;
-//     }
-//     return (
-//       <div>
-//         <EarthMap />
-//         {/* @ts-ignore: */}
-//         <MarsMap weatherLast={this.state.marsWeatherLast} />
-//         {/* @ts-ignore: */}
-//         <Mars weather={this.state.marsWeather} />
-//       </div>
-//     );
-//   }
-// }
-
-// export default MarsWeather;

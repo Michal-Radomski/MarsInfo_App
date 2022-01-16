761372f7b3c575a98f6632b9e5387efa9efa543d
Michal Radomski <michalra@op.pl>
2022-01-16 21:56:43 +0100

    LocalWeather fetching done

--- src/components/pages/MarsWeather/LocalWeather.jsx
+++ src/components/pages/MarsWeather/LocalWeather.jsx
@@ -1,14 +1,53 @@
 import React from "react";
 import styled from "styled-components";
+import {connect} from "react-redux";
 
 const DivLocalWeather = styled.div`
-  background-color: yellow;
+  background-color: lightyellow;
   min-width: 150px;
   height: 350px;
 `;
 
-const LocalWeather = () => {
+//* Api Key: ${apiKey} is not hosted on GitHub.com
+const apiKey = process.env.REACT_APP_OpenWeatherMap_API_KEY;
+// console.log("apiKey:", apiKey);
+
+const LocalWeather = (props) => {
+  // console.log("props:", props);
+  const positionObject = {
+    latitude: props?.state?.location?.latitude,
+    longitude: props?.state?.location?.longitude,
+    city: props?.state?.location?.city,
+    country: props?.state?.location?.country,
+  };
+  // console.log("positionObject:", positionObject);
+
+  // Fetching local weather data
+  function fetchWeather(object) {
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${object.latitude}&lon=${object.longitude}&${apiKey}&units=metric`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Weather:", data);
+        setLocalWeather(data);
+      });
+  }
+
+  const [localWeather, setLocalWeather] = React.useState({});
+
+  React.useEffect(() => {
+    if (props.state.location.latitude) {
+      fetchWeather(positionObject);
+    }
+    console.log("localWeather:", localWeather);
+  }, []);
+
   return <DivLocalWeather>Local Weather</DivLocalWeather>;
 };
 
-export default LocalWeather;
+const mapStateToProps = (state) => ({
+  state: state,
+});
+
+export default connect(mapStateToProps, null)(LocalWeather);

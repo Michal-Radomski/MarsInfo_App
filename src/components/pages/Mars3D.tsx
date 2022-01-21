759cb2ec73444fac28898f38f4d50e8be20fa755
Michal Radomski <michalra@op.pl>
2022-01-21 01:28:47 +0100

    Mars 3D - initial commit2

--- src/components/pages/Mars3D.tsx
+++ src/components/pages/Mars3D.tsx
@@ -1,5 +1,19 @@
+import React from "react";
+import {Viewer, Entity} from "resium";
+import {Cartesian3} from "cesium";
+
+const position = Cartesian3.fromDegrees(18, 54, 100);
+const pointGraphics = {pixelSize: 10};
+
 const Mars3D = (): JSX.Element => {
-  return <div>Mars3D</div>;
+  return (
+    <div>
+      Mars3D
+      <Viewer full>
+        <Entity position={position} point={pointGraphics} />
+      </Viewer>
+    </div>
+  );
 };
 
 export default Mars3D;

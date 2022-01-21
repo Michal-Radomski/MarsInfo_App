b600744ed806810f89db9807d3cf251da20605cd
Michal Radomski <michalra@op.pl>
2022-01-21 16:12:22 +0100

    Mars 3D - commit1

--- src/components/pages/Mars3D.tsx
+++ src/components/pages/Mars3D.tsx
@@ -1,13 +1,27 @@
 import React from "react";
 import {Viewer, Entity, PointGraphics, EntityDescription, ImageryLayer} from "resium";
 import {Cartesian3, createWorldTerrain, ArcGisMapServerImageryProvider} from "cesium";
+import * as Cesium from "cesium";
 
-const position = Cartesian3.fromDegrees(18, 54, 100);
+const position = Cartesian3.fromDegrees(77.4508, 18.4447);
 const terrainProvider = createWorldTerrain();
 
-const imageryProvider = new ArcGisMapServerImageryProvider({
-  // url: "//services.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer",
-  url: "https://cartocdn-gusc.global.ssl.fastly.net/opmbuilder/api/v1/map/named/opm-mars-basemap-v0-2/all/{z}/{x}/{y}.png",
+// const imageryProvider = new ArcGisMapServerImageryProvider({
+//   url: "//services.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer",
+// });
+
+var ellipsoidMars = new Cesium.Ellipsoid(3396000, 3396000, 3396000);
+
+const imageryProvider = new Cesium.WebMapServiceImageryProvider({
+  url: "https://planetarymaps.usgs.gov/cgi-bin/mapserv?map=/maps/mars/mars_simp_cyl.map&service=WMS",
+  layers: "MDIM21_color",
+  parameters: {
+    transparent: false,
+    format: "image/png",
+  },
+  tilingScheme: new Cesium.GeographicTilingScheme({ellipsoid: ellipsoidMars}),
+  tileWidth: 512,
+  tileHeight: 512,
 });
 
 const Mars3D = (): JSX.Element => {

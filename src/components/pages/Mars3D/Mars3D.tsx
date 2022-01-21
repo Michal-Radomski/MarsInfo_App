b3c2df22e08ad2d894bcb207848607657940466d
Michal Radomski <michalra@op.pl>
2022-01-21 22:54:35 +0100

    Mars 3D - commit3

--- src/components/pages/Mars3D/Mars3D.tsx
+++ src/components/pages/Mars3D/Mars3D.tsx
@@ -4,18 +4,43 @@ import * as Cesium from "cesium";
 
 import "./Mars3D.module.scss";
 
-const position = Cesium.Cartesian3.fromDegrees(77.4508, 18.4447);
+// Mars Vehicles' positions:
+type Position = [number, number];
+const InSightPosition: Position = [4.5024, 135.6234];
+const CuriosityPosition: Position = [-4.5895, 137.4417];
+const PerseverancePosition: Position = [18.4447, 77.4508];
+
+// Setting the initial position
+const position = Cesium.Cartesian3.fromDegrees(70, 0, 0);
 // const terrainProvider = Cesium.createWorldTerrain();
 
 // const imageryProvider2 = new Cesium.ArcGisMapServerImageryProvider({
 //   url: "//services.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer",
 // });
 
-const ellipsoidMars = new Cesium.Ellipsoid(3396000, 3396000, 3396000);
+const ellipsoidMars = new Cesium.Ellipsoid(3396000, 3376000, 3389500);
+
+const mapProjectionMars = new Cesium.GeographicProjection(ellipsoidMars);
+const globeMars = new Cesium.Globe(ellipsoidMars);
+
+const materialMars = new Cesium.Color(1, 0, 0).withAlpha(0.7);
+
+globeMars.showGroundAtmosphere = false; // necessary for tiles to show in non-earth ellipsoid
+// globeMars.depthTestAgainstTerrain = true;
+
+const optsMars = {
+  mapProjection: mapProjectionMars,
+  globe: globeMars,
+  // baseLayerPicker: false,
+  // skyAtmosphere: false,
+};
+
+// const viewer = new Cesium.Viewer('cesiumContainer', optsMars);
 
 const imageryProvider = new Cesium.WebMapServiceImageryProvider({
   url: "https://planetarymaps.usgs.gov/cgi-bin/mapserv?map=/maps/mars/mars_simp_cyl.map&service=WMS",
   layers: "MDIM21_color",
+  // layers: "MOLA_THEMIS_blend",
   parameters: {
     transparent: false,
     format: "image/png",
@@ -43,6 +68,8 @@ const Mars3D = (): JSX.Element => {
       // infoBox={false}
       navigationHelpButton={false}
       sceneModePicker={false}
+      skyAtmosphere={false}
+      timeline={false}
     >
       <Entity position={position} name="Gdansk">
         <PointGraphics pixelSize={10} />

e270a4b90d156e6925596a89d15089abf470a46f
Michal Radomski <michalra@op.pl>
2022-01-24 22:11:00 +0100

    Credis - initial commit

--- src/components/pages/Credits.tsx
+++ src/components/pages/Credits.tsx
@@ -1,9 +1,39 @@
+import React from "react";
+import {Button} from "react-bootstrap";
+import Modal from "react-bootstrap/Modal";
+
 const Credits = (): JSX.Element => {
+  const [show, setShow] = React.useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
-    <div style={{textAlign: "center"}}>
-      <h1>Credits</h1>
-      <h3>Under Construction...</h3>
-    </div>
+    <>
+      <Button variant="primary" onClick={handleShow}>
+        Launch demo modal
+      </Button>
+
+      <Modal show={show} onHide={handleClose} animation={false}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div style={{textAlign: "center"}}>
+            <h1>Credits</h1>
+            <h3>Under Construction...</h3>
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 };
 

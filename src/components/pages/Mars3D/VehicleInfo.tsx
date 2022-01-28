81607ce24fdd316ec4892183d1558cb8efab2fd6
Michal Radomski <michalra@op.pl>
2022-01-28 02:33:28 +0100

    little updates5

--- src/components/pages/Mars3D/VehicleInfo.tsx
+++ src/components/pages/Mars3D/VehicleInfo.tsx
@@ -1,12 +1,4 @@
-import React from "react";
-import styled from "styled-components";
 import {Table} from "react-bootstrap";
-// import Spinner from "../../../Spinner";
-
-const Div = styled.div`
-  background-color: yellow !important;
-  border: 2px solid red;
-`;
 
 const td_Style: State = {
   fontWeight: "bold",
@@ -19,34 +11,36 @@ const VehicleInfo = ({info, name}: {info: State; name: string}): JSX.Element => 
   // console.log("{info}:", info);
 
   return (
-    <Div>
-      <Table striped bordered hover variant="dark" width="100%">
-        <thead>
-          <tr>
-            <th style={{float: "left"}}>Vehicle Name:</th>
-            <th style={{float: "right", color: "lightgray", fontStyle: "italic"}}>{name}</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Launch Date: </td>
-            <td style={td_Style}>{info.launchDate}</td>
-          </tr>
-          <tr>
-            <td>Landing Site: </td>
-            <td style={td_Style}>{info.landingSite}</td>
-          </tr>
-          <tr>
-            <td>Coordinates: </td>
-            <td style={td_Style}>{info.coordinates}</td>
-          </tr>
-          <tr>
-            <td>Landing Date: </td>
-            <td style={td_Style}>{info.landingDate}</td>
-          </tr>
-        </tbody>
-      </Table>
-    </Div>
+    <Table striped bordered hover variant="dark" width="100%">
+      <thead>
+        <tr>
+          <th>
+            <span style={{color: "lightyellow", fontStyle: "italic", float: "left"}}>Vehicle Name:</span>
+          </th>
+          <th>
+            <span style={{color: "lightyellow", fontStyle: "italic", float: "right"}}>{name}</span>
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Launch Date: </td>
+          <td style={td_Style}>{info.launchDate}</td>
+        </tr>
+        <tr>
+          <td>Landing Site: </td>
+          <td style={td_Style}>{info.landingSite}</td>
+        </tr>
+        <tr>
+          <td>Coordinates: </td>
+          <td style={td_Style}>{info.coordinates}</td>
+        </tr>
+        <tr>
+          <td>Landing Date: </td>
+          <td style={td_Style}>{info.landingDate}</td>
+        </tr>
+      </tbody>
+    </Table>
   );
 };
 

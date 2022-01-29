38d426c6d342222f64efa9e8ba25b9188fe63680
Michal Radomski <michalra@op.pl>
2022-01-29 02:50:24 +0100

    CountryInfoPopover - initial commit3

--- src/components/pages/MarsWeather/CountryInfoPopover.tsx
+++ src/components/pages/MarsWeather/CountryInfoPopover.tsx
@@ -3,8 +3,10 @@ import {useSelector} from "react-redux";
 import {Card, Table} from "react-bootstrap";
 
 const CountryInfoPopover = (): JSX.Element => {
-  const storeData = useSelector((state: State) => state?.rootReducer?.location ?? "No Data") as State;
-  console.log("storeData:", storeData);
+  const storedData = useSelector((state: State) => state?.rootReducer?.location ?? "No Data") as State;
+  // console.log("storedData:", storedData);
+  const {currency_code, country} = storedData;
+  console.log("currency_code, country:", currency_code, country);
 
   return <div>CountryInfoPopover</div>;
 };

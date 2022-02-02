418e6ef21f42e512e621e9a571dcb7fcfc1604bc
Michal Radomski <michalra@op.pl>
2022-02-02 03:05:02 +0100

    modal in progress - commit1

--- src/components/pages/CreditsModal.tsx
+++ src/components/pages/CreditsModal.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-import {Button} from "react-bootstrap";
-import Modal from "react-bootstrap/Modal";
+import {Button, Modal, Tab, Tabs, Card, ListGroup} from "react-bootstrap";
 
 import "../../styles/App.scss";
 
@@ -20,10 +19,23 @@ const CreditsModal = (): JSX.Element => {
           <Modal.Title>Credits and Info</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <div style={{textAlign: "center"}}>
-            <h1>Credits</h1>
-            <h3>Under Construction...</h3>
-          </div>
+          <Tabs defaultActiveKey="APIs">
+            <Tab eventKey="APIs" title="APIs">
+              <Card style={{width: "100%"}}>
+                <Card.Header style={{fontWeight: "bold", fontStyle: "italic"}}>Used APIs to built the App</Card.Header>
+                <ListGroup variant="flush">
+                  <ListGroup.Item>
+                    NASA Astronomy Picture of the Day and Mars Weather Conditions are taken from from{" "}
+                    <Card.Link href="https://api.nasa.gov" target="_blank">
+                      NASA APIs
+                    </Card.Link>
+                    .
+                  </ListGroup.Item>
+                </ListGroup>
+              </Card>
+            </Tab>
+            <Tab eventKey="Other" title="Other"></Tab>
+          </Tabs>
         </Modal.Body>
         <Modal.Footer style={{backgroundColor: "#0B5ED7"}}>
           <Button variant="outline-light" onClick={handleClose} size="lg">

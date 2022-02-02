399c196782c0477f12681aee8b5183b4a2e1ffaf
Michal Radomski <michalra@op.pl>
2022-02-02 03:31:37 +0100

    APIs data are added

--- src/components/pages/CreditsModal.tsx
+++ src/components/pages/CreditsModal.tsx
@@ -3,6 +3,42 @@ import {Button, Modal, Tab, Tabs, Card, ListGroup} from "react-bootstrap";
 
 import "../../styles/App.scss";
 
+const modalData = [
+  {
+    id: 1,
+    content: "NASA Astronomy Picture of the Day and Mars Weather Conditions are taken from from:",
+    link: "https://api.nasa.gov",
+    label: "NASA APIs",
+  },
+  {id: 2, content: "Maps of Mars are provided by:", link: "https://www.openplanetary.org", label: "OpenPlanetary.org"},
+  {id: 3, content: "", link: ""},
+  {
+    id: 4,
+    content: "World Imagery is provided by:",
+    link: "https://services.arcgisonline.com/arcgis/rest/services/World_Imagery/MapServer",
+    label: "ArcGIS",
+  },
+  {id: 5, content: "Local weather conditions are provided by:", link: "https://openweathermap.org", label: "OpenWeatherMap"},
+  {
+    id: 6,
+    content: "Mars Imagery is provided by:",
+    link: "https://www.usgs.gov/centers/astrogeology-science-center/maps",
+    label: "USGS - Astrogeology Science Center",
+  },
+  {
+    id: 7,
+    content: "Covid Data is provided by:",
+    link: "https://github.com/mathdroid/covid-19-api",
+    label: "mathdroid/covid-19-api",
+  },
+  {
+    id: 8,
+    content: "Currency Exchange Rates are provided by:",
+    link: "https://www.exchangerate-api.com",
+    label: "ExchangeRate-API",
+  },
+];
+
 const CreditsModal = (): JSX.Element => {
   const [show, setShow] = React.useState<boolean>(false);
 
@@ -27,6 +63,13 @@ const CreditsModal = (): JSX.Element => {
                   <ListGroup.Item>
                     NASA Astronomy Picture of the Day and Mars Weather Conditions are taken from from{" "}
                     <Card.Link href="https://api.nasa.gov" target="_blank">
+                      NASA APIs
+                    </Card.Link>
+                    .
+                  </ListGroup.Item>
+                  <ListGroup.Item>
+                    xxxxxxxxxxxx{" "}
+                    <Card.Link href="#" target="_blank">
                       NASA APIs
                     </Card.Link>
                     .

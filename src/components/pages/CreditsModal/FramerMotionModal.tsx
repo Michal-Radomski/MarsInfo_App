fa1d571ba975fea2ab76cfa8eded3ed9da059299
Michal Radomski <michalra@op.pl>
2022-02-05 21:23:20 +0100

    FramerMotion in progress - commit6

--- src/components/pages/CreditsModal/FramerMotionModal.tsx
+++ src/components/pages/CreditsModal/FramerMotionModal.tsx
@@ -1,21 +1,6 @@
+import React from "react";
 import styled from "styled-components";
 import {motion, AnimatePresence} from "framer-motion";
-import React from "react";
-
-const ModalBox = styled(motion.div)`
-  position: relative;
-  z-index: 2;
-  width: 400px;
-  height: 200px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background: wheat;
-`;
-
-const ModalContent = styled(motion.div)`
-  color: blue;
-`;
 
 const ModalContainer = styled.div`
   height: 100%;
@@ -23,7 +8,26 @@ const ModalContainer = styled.div`
   background-color: inherit;
   display: flex;
   flex-direction: column;
+  justify-content: start; //* or center
   align-items: center;
+  align-content: center;
+`;
+
+const ModalDiv = styled(motion.div)`
+  position: relative;
+  z-index: 2;
+  width: 400px;
+  height: 200px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  align-content: center;
+  background: wheat;
+  top: 10%;
+`;
+
+const ModalContent = styled(motion.div)`
+  color: blue;
 `;
 
 const RepoModal = ({showModal}: {showModal: boolean}): JSX.Element => {
@@ -32,20 +36,20 @@ const RepoModal = ({showModal}: {showModal: boolean}): JSX.Element => {
     <ModalContainer>
       <AnimatePresence>
         {showModal && (
-          <ModalBox
+          <ModalDiv
             initial={{opacity: 0, y: 60, scale: 0.5}}
             animate={{
               opacity: 1,
               y: 0,
               scale: 1,
-              transition: {type: "spring", stiffness: 400, duration: 1.0},
+              transition: {type: "spring", stiffness: 400, duration: 1.0, delay: 0.5},
             }}
             exit={{opacity: 0, scale: 0.5, transition: {duration: 0.6}}}
           >
             <ModalContent initial={{y: -30, opacity: 0}} animate={{y: 0, opacity: 1, transition: {delay: 1.0}}}>
               <h2>ModalContent</h2>
             </ModalContent>
-          </ModalBox>
+          </ModalDiv>
         )}
       </AnimatePresence>
     </ModalContainer>
@@ -59,7 +63,7 @@ const FramerMotionModal = ({selectedTab}: {selectedTab: string}): JSX.Element =>
 
   React.useEffect(() => {
     selectedTab === "Repo Link" ? setShowModal(true) : setShowModal(false);
-    console.log("selectedTab:", selectedTab);
+    // console.log("selectedTab:", selectedTab);
   }, [selectedTab]);
 
   return (

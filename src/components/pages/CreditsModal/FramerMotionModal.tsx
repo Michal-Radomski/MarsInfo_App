d16d4e517791d943f7ffdec79e9e874f38fc39fd
Michal Radomski <michalra@op.pl>
2022-02-06 17:33:00 +0100

    FramerMotion - refactor2

--- src/components/pages/CreditsModal/FramerMotionModal.tsx
+++ src/components/pages/CreditsModal/FramerMotionModal.tsx
@@ -16,8 +16,8 @@ const ModalContainer = styled.div`
 const ModalDiv = styled(motion.div)`
   position: relative;
   z-index: 2;
-  width: 450px;
-  height: 250px;
+  width: 400px;
+  height: 200px;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -25,6 +25,7 @@ const ModalDiv = styled(motion.div)`
   background: wheat;
   border: 1px solid #0b5ed7;
 `;
+
 const ModalDivInternal = styled(motion.div)`
   position: relative;
   z-index: 3;
@@ -34,7 +35,7 @@ const ModalDivInternal = styled(motion.div)`
   justify-content: center;
   align-items: center;
   align-content: center;
-  background: orange;
+  background: wheat;
   border: 1px solid #0b5ed7;
 `;
 
@@ -70,19 +71,39 @@ const FramerMotionModal = ({selectedTab}: {selectedTab: string}): JSX.Element =>
   // console.log("selectedTab:", selectedTab);
   const [showModal, setShowModal] = React.useState<boolean>(false);
   // console.log("setShowModal:", showModal);
+  const [animationDone, setAnimationDone] = React.useState<boolean>(false);
+
+  function onComplete() {
+    // console.log("animationDone1:", animationDone);
+    setTimeout(() => {
+      setAnimationDone(true);
+      // console.log("animationDone2:", animationDone);
+    }, 400);
+  }
 
   React.useEffect(() => {
-    selectedTab === "Repo Link" ? setShowModal(true) : setShowModal(false);
+    // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+    selectedTab === "Repo Link" ? setShowModal(true) : (setShowModal(false), setAnimationDone(false));
     // console.log("selectedTab:", selectedTab);
-  }, [selectedTab]);
+    // console.log("animationDone:", animationDone);
+  }, [animationDone, selectedTab]);
 
   return (
     <ModalContainer>
       <AnimatePresence>
         {showModal && (
-          <ModalDiv initial={modalTransition.hidden} animate={modalTransition.visible} exit={modalTransition.exit}>
+          <ModalDiv
+            initial={modalTransition.hidden}
+            animate={modalTransition.visible}
+            exit={modalTransition.exit}
+            onAnimationComplete={onComplete}
+            style={{
+              backgroundColor: animationDone ? "transparent" : "wheat",
+              border: animationDone ? "none" : "border: 1px solid #0b5ed7",
+            }}
+          >
             <ModalDivInternal
-              initial={{opacity: 0, y: 60, scale: 0.5}}
+              initial={{opacity: 0, y: 100, scale: 0.5}}
               animate={{
                 opacity: 1,
                 y: 0,
@@ -91,7 +112,15 @@ const FramerMotionModal = ({selectedTab}: {selectedTab: string}): JSX.Element =>
               }}
               exit={{opacity: 0, scale: 0.5, transition: {duration: 0.6}}}
             >
-              <ModalContent initial={{y: -30, opacity: 0}} animate={{y: 0, opacity: 1, transition: {delay: 1.0}}}>
+              <ModalContent
+                initial={{y: -60, opacity: 0}}
+                animate={{
+                  y: [0, 45, -32, 20, -10, 5, -3, -2, 0],
+                  opacity: 1,
+                  transition: {delay: 2.0, duration: 1.5},
+                  rotate: [0, 10, -10, 8, -8, 5, -5, 0],
+                }}
+              >
                 <h2>ModalContent</h2>
               </ModalContent>
             </ModalDivInternal>

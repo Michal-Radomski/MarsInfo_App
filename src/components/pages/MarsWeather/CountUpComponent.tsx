5b3bf08726537853c5c721906494004c7d0e0190
Michal Radomski <michalra@op.pl>
2022-02-17 15:48:03 +0100

    function generateRandomColor() was added

--- src/components/pages/MarsWeather/CountUpComponent.tsx
+++ src/components/pages/MarsWeather/CountUpComponent.tsx
@@ -5,16 +5,25 @@ const CountUpComponent = ({end}: {end: number}): JSX.Element => {
   // console.log("end:", end);
   const [ready, setReady] = React.useState<boolean>(false);
 
+  // Generate random color
+  function generateRandomColor() {
+    let randomColor: string = Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, "0");
+    randomColor = "#" + randomColor;
+    // console.log("randomColor:", randomColor);
+    return randomColor;
+  }
+
   const styles = {
     Active: {
-      color: "#0D6EFD",
-      fontWeight: "normal",
-
+      color: generateRandomColor(),
+      fontWeight: "bold",
       fontStyle: "normal",
     },
     InActive: {
       color: "black",
-      fontWeight: "bold",
+      fontWeight: "900",
       fontStyle: "italic",
     },
   };

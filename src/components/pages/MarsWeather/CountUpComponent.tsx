724703360bde37a019886d9ed3e60afef8d47eda
Michal Radomski <michalra@op.pl>
2022-02-17 01:04:03 +0100

    CounUp - little updates

--- src/components/pages/MarsWeather/CountUpComponent.tsx
+++ src/components/pages/MarsWeather/CountUpComponent.tsx
@@ -3,14 +3,48 @@ import CountUp from "react-countup";
 
 const CountUpComponent = ({end}: {end: number}): JSX.Element => {
   // console.log("end:", end);
+  const [ready, setReady] = React.useState<boolean>(false);
+
+  const styles = {
+    Active: {
+      color: "#0D6EFD",
+      fontWeight: "normal",
+
+      fontStyle: "normal",
+    },
+    InActive: {
+      color: "black",
+      fontWeight: "bold",
+      fontStyle: "italic",
+    },
+  };
+
   return (
-    <React.Fragment>
+    <>
       {end >= 500 ? (
-        <CountUp separator=" " start={0} end={end} delay={0.8} duration={2.5} />
+        <CountUp
+          separator=" "
+          start={0}
+          end={end}
+          delay={0.8}
+          duration={2.5}
+          onEnd={() => setReady(true)}
+          style={!ready ? styles.Active : styles.InActive}
+        />
       ) : (
-        <CountUp separator=" " start={0} end={end} delay={0.5} duration={2.5} decimals={3} decimal="." />
+        <CountUp
+          separator=" "
+          start={0}
+          end={end}
+          delay={0.8}
+          duration={2.5}
+          decimals={3}
+          decimal="."
+          onEnd={() => setReady(true)}
+          style={!ready ? styles.Active : styles.InActive}
+        />
       )}
-    </React.Fragment>
+    </>
   );
 };
 

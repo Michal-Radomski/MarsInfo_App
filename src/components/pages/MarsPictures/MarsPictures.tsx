8f0550d8048ac16bff7b4c893e5d5987dc702e3b
Michal Radomski <michalra@op.pl>
2022-02-18 23:35:03 +0100

    Accordion in progress - commit1

--- src/components/pages/MarsPictures/MarsPictures.tsx
+++ src/components/pages/MarsPictures/MarsPictures.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import axios from "axios";
-import {Accordion, Card} from "react-bootstrap";
+import {Accordion, AccordionContext, Card} from "react-bootstrap";
 import {useAccordionButton} from "react-bootstrap/AccordionButton";
 
 import Spinner from "../../../Spinner";
@@ -66,17 +66,34 @@ const MarsPictures = (): JSX.Element => {
       }
     };
 
-    fetchMarsPictures();
+    // fetchMarsPictures();
   }, []);
 
-  function CustomToggle({children, eventKey}: any) {
-    const decoratedOnClick = useAccordionButton(eventKey, (event) => {
-      console.log("totally custom!");
-      event.stopPropagation();
-    });
+  const styles = {
+    Active: {
+      color: "#0D6EFD",
+      backgroundColor: "#CFE2FF",
+      border: "2px solid #0D6EFD",
+      padding: "4px",
+      width: "100%",
+      height: "100%",
+    },
+    InActive: {
+      color: "black",
+      backgroundColor: "whitesmoke",
+      border: "1px solid grey",
+      padding: "4px",
+      width: "100%",
+      height: "100%",
+    },
+  };
 
+  function CustomToggle({children, eventKey, callback}: any): JSX.Element {
+    const {activeEventKey} = React.useContext(AccordionContext);
+    const customAccordionOnClick = useAccordionButton(eventKey, () => callback && callback(eventKey));
+    const isCurrentEventKey = activeEventKey === eventKey;
     return (
-      <div style={{backgroundColor: "pink"}} onClick={decoratedOnClick}>
+      <div onClick={customAccordionOnClick} style={isCurrentEventKey ? styles.Active : styles.InActive}>
         {children}
       </div>
     );
@@ -85,48 +102,37 @@ const MarsPictures = (): JSX.Element => {
   return isLoading ? (
     <Spinner />
   ) : (
-    // <Accordion as="div">
-    //   <Accordion.Item eventKey="accordionTab1" as="div">
-    //     <Accordion.Header as="div">
-    //       <div id="test">
-    //         <h1>Pictures from Curiosity Mars Rover</h1>
-    //         <h2>Deployed 6 August 2012</h2>
-
-    //         <PictureDatePicker />
-    //       </div>
-    //     </Accordion.Header>
-    //     <Accordion.Body>Accordion Body Text2</Accordion.Body>
-    //   </Accordion.Item>
-    //   <Accordion.Item eventKey="accordionTab2">
-    //     <Accordion.Header as="div">Accordion Item #2</Accordion.Header>
-    //     <Accordion.Body>Accordion Body Text2</Accordion.Body>
-    //   </Accordion.Item>
-    // </Accordion>
-
-    <Accordion defaultActiveKey="0">
-      <Card>
-        <Card.Header>
-          <CustomToggle eventKey="0">
-            Click me!
+    <Accordion as="div" defaultActiveKey="accordionTab1" style={{border: "1px solid #0D6EFD", margin: "4px"}}>
+      <Card as="div">
+        <Card.Header as="div" style={{padding: 0}}>
+          <CustomToggle eventKey="accordionTab1">
             <div>
               <h1>Pictures from Curiosity Mars Rover</h1>
               <h2>Deployed 6 August 2012</h2>
-              <div onClick={(event: {stopPropagation: () => any}) => event.stopPropagation()}>
+              <div onClick={(event: {stopPropagation: () => void}) => event.stopPropagation()}>
                 <PictureDatePicker />
               </div>
             </div>
           </CustomToggle>
         </Card.Header>
-        <Accordion.Collapse eventKey="0">
-          <Card.Body>Hello! I'm the body</Card.Body>
+        <Accordion.Collapse eventKey="accordionTab1" as="div">
+          <Card.Body as="div">Hello! I'm the body Card</Card.Body>
         </Accordion.Collapse>
       </Card>
-      <Card>
-        <Card.Header>
-          <CustomToggle eventKey="1">Click me!</CustomToggle>
+      <Card as="div">
+        <Card.Header as="div" style={{padding: 0}}>
+          <CustomToggle eventKey="accordionTab2">
+            <div>
+              <h1>Pictures from Curiosity Mars Rover</h1>
+              <h2>Deployed 6 August 2012</h2>
+              <div onClick={(event: {stopPropagation: () => void}) => event.stopPropagation()}>
+                <PictureDatePicker />
+              </div>
+            </div>
+          </CustomToggle>
         </Card.Header>
-        <Accordion.Collapse eventKey="1">
-          <Card.Body>Hello! I'm another body</Card.Body>
+        <Accordion.Collapse eventKey="accordionTab2">
+          <Card.Body>Hello! I'm another body Card2</Card.Body>
         </Accordion.Collapse>
       </Card>
     </Accordion>
@@ -134,45 +140,3 @@ const MarsPictures = (): JSX.Element => {
 };
 
 export default MarsPictures;
-
-// React.useEffect(() => {
-//   async function fetchData() {
-//     // console.log("fetching covidData and currencyData");
-//     const [covidByCountry, covidGlobal, rates] = await Promise.allSettled([
-//       fetch(`https://covid19.mathdro.id/api/countries/${country}`).then((response) => response.json()),
-//       fetch("https://covid19.mathdro.id/api").then((response) => response.json()),
-//       fetch(`https://open.er-api.com/v6/latest/${currency_code}`).then((response) => response.json()),
-//     ]);
-//     // console.log("covidByCountry:", covidByCountry);
-//     // console.log("covidGlobal:", covidGlobal);
-//     // console.log("rates:", rates);
-//     const result = {
-//       //@ts-ignore
-//       countryCovid: covidByCountry.value,
-//       //@ts-ignore
-//       globalCovid: covidGlobal.value,
-//       //@ts-ignore
-//       currencyRates: rates.value,
-//     };
-//     // console.log("result:", result);
-//     const covidData = {
-//       globalConfirmed: result?.globalCovid?.confirmed?.value ?? "No Data",
-//       globalDeaths: result?.globalCovid?.deaths?.value ?? "No Data",
-//       countryConfirmed: result?.countryCovid?.confirmed?.value ?? "No Data",
-//       countryDeaths: result?.countryCovid?.deaths?.value ?? "No Data",
-//       lastUpdate: result?.globalCovid?.lastUpdate || result?.countryCovid?.lastUpdate || "No Data",
-//     };
-//     const currencyData = {
-//       lastUpdate: result?.currencyRates?.time_last_update_utc ?? "No Data",
-//       to_Selected: result?.currencyRates?.rates[`${currency_code}`] ?? "No Data",
-//       to_USD: result?.currencyRates?.rates?.USD ?? "No Data",
-//       to_EURO: result?.currencyRates?.rates?.EUR ?? "No Data",
-//       to_CHF: result?.currencyRates?.rates?.CHF ?? "No Data",
-//     };
-//     // await console.log("covidData, currencyData:", covidData, currencyData);
-//     await dispatch({type: GET_COVID_DATA, payload: covidData});
-//     await dispatch({type: GET_RATES_DATA, payload: currencyData});
-//     // setting the items to the localStorage
-//     await localStorage.setItem("covidData", JSON.stringify(covidData));
-//     await localStorage.setItem("currencyData", JSON.stringify(currencyData));
-//   }

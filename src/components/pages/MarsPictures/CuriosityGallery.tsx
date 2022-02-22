8337fce6afda5d38f5d0d58fcd5d722b758fb6f0
Michal Radomski <michalra@op.pl>
2022-02-22 22:12:52 +0100

    Curiosity Gallery - commit2

--- src/components/pages/MarsPictures/CuriosityGallery.tsx
+++ src/components/pages/MarsPictures/CuriosityGallery.tsx
@@ -1,30 +1,22 @@
 import React from "react";
 
 const CuriosityGallery = ({pictures}: State) => {
-  if (typeof pictures !== "string") {
-    // console.log("pictures.photos:", pictures?.photos);
-
-    const picturesGallery = pictures?.photos.map((picture: any) => {
-      return (
-        <div>
-          <img width="200px" src={picture.img_src} alt={picture.id}></img>
-          <p>
-            Name of Camera: {picture.camera.full_name} ({picture.camera.name})
-          </p>
-          <p>Earth date: {picture.earth_date}</p>
-          <p>Sol: {picture.sol}</p>
-        </div>
-      );
-    });
-
-    return picturesGallery;
-  }
-
   const curiosityPhotos =
     typeof pictures === "string" ? (
       <h3 style={{textAlign: "center", fontWeight: "bold", color: "red"}}>{pictures}</h3>
     ) : (
-      <>picturesGallery</>
+      pictures?.photos.map((picture: any) => {
+        return (
+          <div key={picture.id}>
+            <img width="200px" src={picture.img_src} alt={picture.id}></img>
+            <p>
+              Name of Camera: {picture.camera.full_name} ({picture.camera.name})
+            </p>
+            <p>Earth date: {picture.earth_date}</p>
+            <p>Sol: {picture.sol}</p>
+          </div>
+        );
+      })
     );
   // console.log("curiosityPhotos:", curiosityPhotos);
 

e053fc87f0a551065c8911efa1bb5e6c767ef738
Michal Radomski <michalra@op.pl>
2022-02-28 19:26:33 +0100

    NasaMarsModel3D - commit1

---
+++ src/components/pages/NasaModel/Model3D.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import {useFrame} from "@react-three/fiber";
+import {OrbitControls, Stars} from "@react-three/drei";
+import * as THREE from "three";
+
+const Model3D: React.FC<{}> = (): JSX.Element => {
+  const earthRef = React.useRef<THREE.Mesh>();
+
+  useFrame(({clock}) => {
+    const elapsedTime = clock.getElapsedTime();
+    // console.log("elapsedTime:", elapsedTime);
+    earthRef!.current!.rotation.y = elapsedTime / 15;
+  });
+
+  React.useEffect(() => {
+    console.log("earthRef:", earthRef);
+  });
+
+  return (
+    <React.Fragment>
+      {/* <ambientLight intensity={0.9} color={0xffffff} /> */}
+      <pointLight color="#f6f3ea" position={[2, 0, 5]} intensity={1.2} />
+      <Stars
+        radius={200} // Radius of the inner sphere (default=100)
+        depth={60} // Depth of area where stars should fit (default=50)
+        count={10000} // Amount of stars (default=5000)
+        factor={7} // Size factor (default=4)
+        saturation={0} // Saturation 0-1 (default=0)
+        fade={true} // Faded dots (default=false)
+      />
+
+      <mesh ref={earthRef} position={[0, 0, 3]}>
+        <sphereGeometry args={[1, 32, 32]} />
+        <meshPhongMaterial />
+        <meshStandardMaterial metalness={0.4} roughness={0.7} />
+        <OrbitControls
+          enableZoom={true}
+          enablePan={true}
+          enableRotate={true}
+          zoomSpeed={0.6}
+          panSpeed={0.5}
+          rotateSpeed={0.4}
+        />
+      </mesh>
+    </React.Fragment>
+  );
+};
+
+export default Model3D;
--- src/components/pages/NasaModel/NasaModel.tsx
+++ src/components/pages/NasaModel/NasaModel.tsx
@@ -1,7 +1,56 @@
 import React from "react";
+import styled from "styled-components";
+import {Canvas} from "@react-three/fiber";
 
-const NasaModel = () => {
-  return <React.Fragment>NasaModel</React.Fragment>;
-};
+import "./NasaModel.scss";
+import Model3D from "./Model3D";
+
+const CanvasContainer = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+const Background = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  background-color: #1756dd31; //*  #RRGGBBAA => rgba(23, 86, 221, 0.192)
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  z-index: 9;
+`;
+
+//* WebGL compatibility check
+if (typeof WebGLRenderingContext === "undefined") {
+  // This browser doesn't even know what WebGL is
+  console.log("This browser doesn't even know what WebGL is");
+} else {
+  let WebGL = document.createElement("canvas").getContext("webgl");
+  console.log("WebGL:", WebGL);
+  if (!WebGL) {
+    // webgl failed to initialize for any number of reasons
+    // including it's turned off, the browser blacklisted the drivers,
+    // it's out of memory, other.
+    console.log("WebGL failed to initialize");
+  }
+}
+
+function NasaModel(): JSX.Element {
+  return (
+    <div style={{backgroundColor: "#010718"}}>
+      <CanvasContainer>
+        <Background />
+        <Canvas>
+          <React.Suspense fallback={null}>
+            <Model3D />
+          </React.Suspense>
+        </Canvas>
+      </CanvasContainer>
+    </div>
+  );
+}
 
 export default NasaModel;

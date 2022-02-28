21857c05c23309a08d68113f5d3f3bd49a165c05
Michal Radomski <michalra@op.pl>
2022-02-28 02:28:04 +0100

    temp commit4

--- src/components/pages/MarsPictures/MarsPictures.tsx
+++ src/components/pages/MarsPictures/MarsPictures.tsx
@@ -47,13 +47,27 @@ const DivInner = styled.div`
   position: relative;
 `;
 
+// React.useEffect(() => {
+//   if (accordionActiveTab === null || accordionActiveTab === "accordionTab1" || accordionActiveTab === "accordionTab2") {
+//     setFinalActiveTab(accordionActiveTab);
+//   }
+
+//   if (localStorage.hasOwnProperty("accordionActiveTab")) {
+//     const activeTabFromLocalStorage = JSON.parse(localStorage.getItem("accordionActiveTab") as string);
+//     setFinalActiveTab(activeTabFromLocalStorage);
+//   } else setFinalActiveTab("accordionTab1");
+//   console.log("finalActiveTab:", finalActiveTab);
+// }, [accordionActiveTab, finalActiveTab]);
+
 const MarsPictures = (): JSX.Element => {
   const [isLoading, setIsLoading] = React.useState<boolean>(true);
+  const [finalActiveTab, setFinalActiveTab] = React.useState<string | null>("accordionTab1");
 
   const dispatch: Dispatch = useDispatch();
 
   const accordionActiveTab: string | null = useSelector((state: State) => state?.rootReducer?.MarsPictures?.activeTab);
-  //  console.log("accordionActiveTab:", accordionActiveTab)
+  // console.log("accordionActiveTab:", accordionActiveTab);
+  setFinalActiveTab(accordionActiveTab);
 
   const MarsDateCuriosity: string = useSelector(
     (state: State) => state?.rootReducer?.MarsPictures_Curiosity?.CuriosityRoverDate
@@ -201,13 +215,18 @@ const MarsPictures = (): JSX.Element => {
       localStorage.setItem("MarsPicturesCuriosity", JSON.stringify(MarsPicturesCuriosity));
       localStorage.setItem("MarsDateInActiveRovers", JSON.stringify(MarsDateInActiveRovers));
       localStorage.setItem("MarsPicturesInActiveRovers", JSON.stringify(MarsPicturesInActiveRovers));
-    }, 800);
+      localStorage.setItem("accordionActiveTab", JSON.stringify(accordionActiveTab));
+    }, 500);
   });
 
   return isLoading ? (
     <Spinner />
   ) : (
-    <Accordion as="div" defaultActiveKey="accordionTab1" style={{border: "1px solid #0D6EFD", margin: "6px", padding: 0}}>
+    <Accordion
+      as="div"
+      defaultActiveKey={finalActiveTab as string}
+      style={{border: "1px solid #0D6EFD", margin: "6px", padding: 0}}
+    >
       <Card as="div" style={{border: "1px solid #0D6EFD", margin: "0px", padding: 0}}>
         <Card.Header as="div" style={{padding: 0, margin: 0}}>
           <CustomToggle eventKey="accordionTab1">
@@ -215,7 +234,7 @@ const MarsPictures = (): JSX.Element => {
             <Div>
               <DivInner>
                 <div style={{top: "50%", position: "absolute", transform: "translateY(-50%)"}}>
-                  {accordionActiveTab === "accordionTab1" ? (
+                  {finalActiveTab === "accordionTab1" ? (
                     <FaMinusSquare size={32} fill="indigo" />
                   ) : (
                     <FaPlusSquare size={32} fill="green" />
@@ -275,7 +294,7 @@ const MarsPictures = (): JSX.Element => {
             <Div>
               <DivInner>
                 <div style={{top: "50%", position: "absolute", transform: "translateY(-50%)"}}>
-                  {accordionActiveTab === "accordionTab2" ? (
+                  {finalActiveTab === "accordionTab2" ? (
                     <FaMinusSquare size={32} fill="indigo" />
                   ) : (
                     <FaPlusSquare size={32} fill="green" />
